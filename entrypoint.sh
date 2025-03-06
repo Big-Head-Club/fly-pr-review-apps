5e9b53e0deca7881d72a7d99a897e2665d9e518d
DJViau <djviau@users.noreply.github.com>
2025-03-06 12:51:26 -0500

    maybe fix the failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,12 +35,6 @@ node_env="$INPUT_NODE_ENV"
 echo "vite_use_dummy_data=$vite_use_dummy_data"
 echo "node_env=$node_env"
 
-# Error out if any of the variables are empty
-if [ -z "$vite_use_dummy_data" ] || [ -z "$node_env" ]; then
-  echo "Error: One or more variables are empty. Please check your environment variables."
-  exit 1
-fi
-
 if ! echo "$app" | grep "$PR_NUMBER"; then
   echo "For safety, this action requires the app's name to contain the PR number."
   exit 1
@@ -109,9 +103,9 @@ fi
 # Trigger the deploy of the new version.
 echo "Contents of config $config file: " && cat "$config"
 if [ -n "$INPUT_VM" ]; then
-  flyctl deploy --local-only --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE" --dockerfile activity-server.Dockerfile --build-arg VITE_API_URL=https://mop-pr-$PR_NUMBER.fly.dev --build-arg VITE_SHOULD_PROXY_S3=false --build-arg VITE_USE_DUMMY_DATA="$vite_use_dummy_data" --build-arg NODE_ENV=development
+  flyctl deploy --local-only --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE" --dockerfile activity-server.Dockerfile --build-arg VITE_API_URL=https://mop-pr-$PR_NUMBER.fly.dev --build-arg VITE_SHOULD_PROXY_S3=false --build-arg VITE_USE_DUMMY_DATA="false" --build-arg NODE_ENV=development
 else
-  flyctl deploy --local-only --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY" --dockerfile activity-server.Dockerfile --build-arg VITE_API_URL=https://mop-pr-$PR_NUMBER.fly.dev --build-arg VITE_SHOULD_PROXY_S3=false --build-arg VITE_USE_DUMMY_DATA="$vite_use_dummy_data" --build-arg NODE_ENV=development
+  flyctl deploy --local-only --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY" --dockerfile activity-server.Dockerfile --build-arg VITE_API_URL=https://mop-pr-$PR_NUMBER.fly.dev --build-arg VITE_SHOULD_PROXY_S3=false --build-arg VITE_USE_DUMMY_DATA="false" --build-arg NODE_ENV=development
 fi
 
 # Make some info available to the GitHub workflow.
